26b9d6f01a9834b103f024cdc0c62fab1cff04c5
manikumar <manikumar.marre@gmail.com>
2023-08-01 03:36:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// const { add, sub } = require("./calculator.js");
+const a = require("./calculator");
+// console.log("Add : " + add(2, 6));
+// console.log("Sub : " + sub(4, 2));
+console.log(a);
+console.log(a + a);
---
+++ index.mjs
@@ -0,0 +1,4 @@
+import { add, sub } from "./calculator.mjs";
+
+console.log(add(4, 6));
+console.log(sub(6, 5));
